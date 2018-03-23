29d20d00a00f397cbe5e7dc1de65035fc871d43b
Student15 <theblackboat39@gmail.com>
2018-03-23 16:10:07 +0700

    16.10 น.

--- app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
+++ app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
@@ -1,24 +1,49 @@
 package th.ac.lannapoly.polytechlanna
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,
+        Home.OnFragmentInteractionListener ,
+        ContactFragment.OnFragmentInteractionListener{
+
+     var homeFragment:Home? = null
+     var contactFragment:ContactFragment? = null
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                if (homeFragment == null){
+                    homeFragment = Home()
+                }
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frameLayout, homeFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_giftcard -> {
+                if (contactFragment == null){
+                    contactFragment = ContactFragment()
+                }
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frameLayout, contactFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +55,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        homeFragment = Home()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, homeFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
